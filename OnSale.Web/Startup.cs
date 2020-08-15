d344341bb57a21bbc1c0ac55f30d079fcd464726
Jimmy Davila <davila56jd@gmail.com>
2020-08-15 13:15:47 -0400

    Added the SeedDB Class in order to have some data if I have to drop the database

--- OnSale.Web/Startup.cs
+++ OnSale.Web/Startup.cs
@@ -32,6 +32,8 @@ namespace OnSale.Web
             {
                 cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddTransient<SeedDb>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
